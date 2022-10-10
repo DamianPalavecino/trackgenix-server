be3c6558dcbf18a3760b4855b7a5d2e09d672a05
Fabian Dutra Silva <fabiandutrasilva@gmail.com>
2022-10-10 03:24:51 -0300

    TG: fix bug in getAll path, creating /employees path

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
+const employeeRouter = require('./resources/employees');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/employees', employeeRouter);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
