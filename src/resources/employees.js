e19f46b188d493c0fb010436f318a7ae55178025
juancapp <juanitocamo3@gmail.com>
2022-10-09 22:44:59 -0300

    TG: getById request

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -8,4 +8,16 @@ router.get('/getAll', (req, res) => {
   res.send(employees);
 });
 
+router.get('/getById/:id', (req, res) => {
+  const employeeId = req.params.id;
+  const foundEmployee = employees.find(
+    (employee) => employee.id === employeeId,
+  );
+  if (foundEmployee) {
+    res.send(foundEmployee);
+  } else {
+    res.send('Employee not found');
+  }
+});
+
 module.exports = router;
