eeed188b072d0ad1fe01dce5d96c184d350f7131
juancapp <juanitocamo3@gmail.com>
2022-10-09 22:49:01 -0300

    TG: post request

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const fs = require('fs');
+const fs = require('fs');
 const employees = require('../data/employees.json');
 
 const router = express.Router();
@@ -18,6 +18,18 @@ router.get('/getById/:id', (req, res) => {
   } else {
     res.send('Employee not found');
   }
+});
+
+router.post('/add', (req, res) => {
+  const newEmployee = req.body;
+  employees.push(newEmployee);
+  fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
+    if (err) {
+      res.send('Cannot save new employee');
+    } else {
+      res.send('Employee created');
+    }
+  });
 });
 
 module.exports = router;
