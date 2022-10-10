11dcf3c0a9cc8bc1f9796ee18d8f91d1d7070c2b
Fabian Dutra Silva <fabiandutrasilva@gmail.com>
2022-10-10 03:34:14 -0300

    TG: employee add feature completely operative

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const fs = require('fs');
 const employees = require('../data/employees.json');
 
 const router = express.Router();
 
 router.get('/getAll', (req, res) => {
   res.send(employees);
+});
+
+router.post('/add', (req, res) => {
+  const newEmployee = req.body;
+  employees.push(newEmployee);
+  fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
+    if (err) {
+      res.send('Cannot save new employee');
+    } else {
+      res.send('Employee created');
+    }
+  });
 });
 
 module.exports = router;
