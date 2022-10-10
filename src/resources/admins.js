e93286555f8f07b2e352762a1d8b50af94e9f9b1
Luis Queffelec <queffelecluis@gmail.com>
2022-10-10 20:58:01 -0300

    TG: created the delete and edit admins logic

---
+++ src/resources/admins.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+const admins = require('../data/admins.json');
+
+const router = express.Router();
+
+router.delete('/delete/:id', (req, res) => {
+  const adminId = req.params.id;
+  const filteredadmins = admins.filter((admin) => admin.id !== adminId);
+  fs.writeFile('src/data/admins.json', JSON.stringify(filteredadmins), (err) => {
+    if (err) {
+      res.send('Cannot delete admin');
+    } else {
+      res.send('admin deleted');
+    }
+  });
+});
+
+router.put('/change/:id', (req, res) => {
+  const adminId = req.params.id;
+  const newAdmin = req.body;
+  const foundAdmin = admins.find(
+    (admin) => admin.id === adminId,
+  );
+  const index = admins.findIndex((admin) => admin.id === adminId);
+  if (newAdmin.name) {
+    foundAdmin.name = newAdmin.name;
+  }
+  if (newAdmin.lastName) {
+    foundAdmin.name = newAdmin.lastName;
+  }
+  if (newAdmin.email) {
+    foundAdmin.name = newAdmin.email;
+  }
+  admins[index] = foundAdmin;
+  fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
+    if (err) {
+      res.send('Cannot modify admin');
+    } else {
+      res.send('Admin modified');
+    }
+  });
+});
+
+module.exports = router;
