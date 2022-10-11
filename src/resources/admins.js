84999624010b33638618082ddbb53c10f0076505
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-10-11 18:27:39 -0300

    TG: Add admins getById

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -7,4 +7,14 @@ router.get('/getAll', (req, res) => {
   res.send(admins);
 });
 
+router.get('/getById/:id', (req, res) => {
+  const adminId = req.params.id;
+  const adminFound = admins.find((admin) => admin.id === adminId);
+  if (adminFound) {
+    res.send(adminFound);
+  } else {
+    res.send('Cannot find admin');
+  }
+});
+
 module.exports = router;
