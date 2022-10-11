43e1d8bd44c47861abe2b485b3f08849da01e55c
Fabian Dutra Silva <fabiandutrasilva@gmail.com>
2022-10-10 22:50:38 -0300

    TG: getByRate feature added

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,41 +1,24 @@
 const express = require('express');
-const fs = require('fs');
 const employees = require('../data/employees.json');
 
 const router = express.Router();
 
-router.get('/getAll', (req, res) => {
-  res.send(employees);
-});
-
-router.post('/add', (req, res) => {
-  const newEmployee = req.body;
-  employees.push(newEmployee);
-  fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
-    if (err) {
-      res.send('Cannot save new employee');
-    } else {
-      res.send('Employee created');
-    }
-  });
-});
-
-router.delete('/delete/:id', (req, res) => {
-  const employeeId = req.params.id;
-  const filteredEmployees = employees.filter(
-    (employee) => employee.id !== employeeId,
-  );
-  fs.writeFile(
-    'src/data/employees.json',
-    JSON.stringify(filteredEmployees),
-    (err) => {
-      if (err) {
-        res.send('Cannot delete employee');
-      } else {
-        res.send('Employee deleted');
+router.get('/getByRate/:rate', (req, res) => {
+  const { rate } = req.params;
+  const rateNumber = Number(rate);
+  const newArray = [];
+  employees.forEach((employee) => {
+    employee.projects.forEach((project) => {
+      if (project.rate === rateNumber) {
+        newArray.push(employee);
       }
-    },
-  );
+    });
+  });
+  if (newArray) {
+    res.send(newArray);
+  } else {
+    res.send('Employee not found');
+  }
 });
 
 module.exports = router;
