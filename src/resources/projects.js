a5591f40e419f6b200ca2fc2793f712c76b063c6
CrearDev1 <stc.enprend@hotmail.com>
2022-10-11 19:38:37 -0300

    TG:Create editByRole and Add Employee

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -5,12 +5,6 @@ const projects = require('../data/projects.json');
 
 const router = express.Router();
 
-// const createProject = () => {};
-// const editProject = () => {};
-// const deleteProject = () => {};
-// const addEmployeeProject = () => {};
-
-// const getProject = () => {};
 router.get('/getById/:id', (req, res) => {
   const projectId = req.params.id;
   const foundProject = projects.find((project) => project.id === projectId);
@@ -21,7 +15,6 @@ router.get('/getById/:id', (req, res) => {
   }
 });
 
-// const listProject = () => {};
 router.get('/getAll', (req, res) => {
   res.send(projects);
 });
@@ -31,7 +24,7 @@ router.post('/add', (req, res) => {
   projects.push(newProject);
   fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
     if (err) {
-      res.send('Cannot save new user');
+      res.send('Cannot save new project');
     } else {
       res.send('Project create');
     }
@@ -50,4 +43,67 @@ router.delete('/deleted/:id', (req, res) => {
   });
 });
 
+router.put('/edit/:id', (req, res) => {
+  const projectId = req.params.id;
+  const newProject = req.body;
+  const foundProject = projects.find(
+    (project) => project.id === projectId,
+  );
+  const i = projects.findIndex((project) => project.id === projectId);
+  if (newProject.name) {
+    foundProject.name = newProject.name;
+  }
+  if (newProject.password) {
+    foundProject.password = newProject.password;
+  }
+  if (newProject.id) {
+    foundProject.id = newProject.id;
+  }
+  projects[i] = foundProject;
+  fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
+    if (err) {
+      res.send('Cannot edit project');
+    } else {
+      res.send('Employee edited');
+    }
+  });
+});
+
+router.put('/addE/:id', (req, res) => {
+  const projectId = req.params.id;
+  const newEmployee = req.body;
+  const foundProject = projects.find(
+    (project) => project.id === projectId,
+  );
+  const { members } = foundProject;
+  if (members.find((member) => member.id === newEmployee.id)) {
+    res.send('Employee already exist');
+  } else {
+    foundProject.members.push(newEmployee);
+    fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
+      if (err) {
+        res.send('Cannot add employee');
+      } else {
+        res.status(200).send({ message: 'Employee added' });
+      }
+    });
+  }
+});
+
+router.get('/getByRole/:role', (req, res) => {
+  const { role } = req.params;
+  const newArray = [];
+  projects.forEach((project) => {
+    project.members.forEach((member) => {
+      if (member.role === role) {
+        newArray.push(project);
+      }
+    });
+  });
+  if (newArray) {
+    res.send(newArray);
+  } else {
+    res.send('Employee not found');
+  }
+});
 module.exports = router;
