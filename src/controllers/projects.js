ef52a61612ee8f4f5035a3373f87a09e430f6660
juancapp <juanitocamo3@gmail.com>
2022-10-17 03:18:10 -0300

    getById, getAll and create

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,113 +1,84 @@
-const express = require('express');
-const fs = require('fs');
-const projects = require('../data/projects.json');
+import Projects from '../models/Projects';
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send(projects);
-});
-
-router.get('/:id', (req, res) => {
-  const projectId = req.params.id;
-  const projectFound = projects.find((project) => project.id === projectId);
-  if (projectFound) {
-    res.send(projectFound);
-  } else {
-    res.send('project not found');
+const createProject = async (req, res) => {
+  try {
+    const project = new Projects({
+      employees: req.body.employees,
+      name: req.body.name,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      clientName: req.body.clientName,
+      description: req.body.description,
+    });
+    const result = project.save((error, dataProject) => {
+      if (error) {
+        return res.status(400).json({
+          message: error,
+          data: undefined,
+          error: true,
+        });
+      }
+      return res.status(201).json({
+        message: 'Project created',
+        data: dataProject,
+        error: false,
+      });
+    });
+    return result;
+  } catch (error) {
+    return res.status(400).json({
+      message: 'An error ocurred',
+      data: undefined,
+      error: true,
+    });
   }
-});
+};
 
-router.post('/', (req, res) => {
-  const newProject = req.body;
-  projects.push(newProject);
-  fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-    if (err) {
-      res.send('cannot save project');
-    } else {
-      res.send('project created');
+const getAllProjects = async (req, res) => {
+  try {
+    const projects = await Projects.find();
+
+    return res.status(200).json({
+      message: 'Project found',
+      data: projects,
+      error: false,
+    });
+  } catch (error) {
+    return res.json({
+      message: 'An error ocurred',
+      error: true,
+    });
+  }
+};
+
+const getProjectById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const projects = await Projects.findById(id);
+
+    if (projects == null) {
+      return res.status(404).json({
+        message: 'Project not found',
+        data: undefined,
+        error: true,
+      });
     }
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const projectId = req.params.id;
-  const foundProjects = projects.find((project) => project.id === projectId);
-  if (!foundProjects) {
-    res.send('Project not found');
-  } else {
-    const filteredProjects = projects.filter((project) => project.id !== projectId);
-    fs.writeFile('src/data/projects.json', JSON.stringify(filteredProjects), (err) => {
-      if (err) {
-        res.send('Cannot delete project');
-      } else {
-        res.send('Project deleted');
-      }
+    return res.status(200).json({
+      message: 'Project found',
+      data: projects,
+      error: false,
+    });
+  } catch (error) {
+    return res.json({
+      message: `An error ocurred: ${error}`,
+      error: true,
+      data: undefined,
     });
   }
-});
+};
 
-router.put('/edit/:id', (req, res) => {
-  const projectId = req.params.id;
-  const newProject = req.body;
-  const foundProject = projects.find(
-    (project) => project.id === projectId,
-  );
-  const i = projects.findIndex((project) => project.id === projectId);
-  if (newProject.name) {
-    foundProject.name = newProject.name;
-  }
-  if (newProject.password) {
-    foundProject.password = newProject.password;
-  }
-  if (newProject.id) {
-    foundProject.id = newProject.id;
-  }
-  projects[i] = foundProject;
-  fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-    if (err) {
-      res.send('Cannot edit project');
-    } else {
-      res.send('Employee edited');
-    }
-  });
-});
-
-router.put('/addE/:id', (req, res) => {
-  const projectId = req.params.id;
-  const newEmployee = req.body;
-  const foundProject = projects.find(
-    (project) => project.id === projectId,
-  );
-  const { members } = foundProject;
-  if (members.find((member) => member.id === newEmployee.id)) {
-    res.send('Employee already exist');
-  } else {
-    foundProject.members.push(newEmployee);
-    fs.writeFile('src/data/projects.json', JSON.stringify(projects), (err) => {
-      if (err) {
-        res.send('Cannot add employee');
-      } else {
-        res.status(200).send({ message: 'Employee added' });
-      }
-    });
-  }
-});
-
-router.get('/getByRole/:role', (req, res) => {
-  const { role } = req.params;
-  const newArray = [];
-  projects.forEach((project) => {
-    project.members.forEach((member) => {
-      if (member.role === role) {
-        newArray.push(project);
-      }
-    });
-  });
-  if (newArray) {
-    res.send(newArray);
-  } else {
-    res.send('Employee not found');
-  }
-});
-module.exports = router;
+export default {
+  createProject,
+  getAllProjects,
+  getProjectById,
+};
