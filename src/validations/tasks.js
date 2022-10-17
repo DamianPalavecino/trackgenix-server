eee26c9ba1c24b97d12514ad936508c2e1b15c92
Valentín Michilena <valentin.michilena@gmail.com>
2022-10-17 06:05:49 -0300

    TG: Added validation to edit.

--- src/validations/tasks.js
+++ src/validations/tasks.js
@@ -16,7 +16,24 @@ const validateCreation = (req, res, next) => {
   }
   return next();
 };
+const validateEdition = (req, res, next) => {
+  const taskValidation = Joi.object({
+    description: Joi.string().min(6).max(100),
+  });
+
+  const validation = taskValidation.validate(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: `There was an error: ${validation.error.details[0].message}`,
+      data: undefined,
+      error: true,
+    });
+  }
+  return next();
+};
 
 export default {
   validateCreation,
+  validateEdition,
 };
