eaca6f14c881f49b0105f58ff67523a9e80b06f4
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-10-17 20:41:14 -0300

    TG: Corrected status codes

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,107 +1,16 @@
-// const express = require('express');
-// const fs = require('fs');
-// const employees = require('../data/employees.json');
-
-// const router = express.Router();
-
-// router.get('/', (req, res) => {
-//   res.send(employees);
-// });
-
-// router.get('/:id', (req, res) => {
-//   const employeeId = req.params.id;
-//   const foundEmployee = employees.find(
-//     (employee) => employee.id === employeeId,
-//   );
-//   if (foundEmployee) {
-//     res.send(foundEmployee);
-//   } else {
-//     res.send('Employee not found');
-//   }
-// });
-
-// router.get('/getByRate/:rate', (req, res) => {
-//   const { rate } = req.params;
-//   const rateNumber = Number(rate);
-//   const newArray = [];
-//   employees.forEach((employee) => {
-//     employee.projects.forEach((project) => {
-//       if (project.rate === rateNumber) {
-//         newArray.push(employee);
-//       }
-//     });
-//   });
-//   if (newArray.length > 0) {
-//     res.send(newArray);
-//   } else {
-//     res.send('Employee not found');
-//   }
-// });
-
-// router.post('/', (req, res) => {
-//   const newEmployee = req.body;
-//   employees.push(newEmployee);
-//   fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
-//     if (err) {
-//       res.send('Cannot save new employee');
-//     } else {
-//       res.send('Employee created');
-//     }
-//   });
-// });
-
-// router.put('/:id', (req, res) => {
-//   const employeeId = req.params.id;
-//   const newEmployee = req.body;
-//   const foundEmployee = employees.find(
-//     (employee) => employee.id === employeeId,
-//   );
-//   const i = employees.findIndex((employee) => employee.id === employeeId);
-//   if (newEmployee.firstName) {
-//     foundEmployee.firstName = newEmployee.firstName;
-//   }
-//   if (newEmployee.lastName) {
-//     foundEmployee.lastName = newEmployee.lastName;
-//   }
-//   if (newEmployee.dni) {
-//     foundEmployee.dni = newEmployee.dni;
-//   }
-//   if (newEmployee.projects) {
-//     foundEmployee.projects = newEmployee.projects;
-//   }
-//   employees[i] = foundEmployee;
-//   fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
-//     if (err) {
-//       res.send('Cannot change employee');
-//     } else {
-//       res.send('Employee changed');
-//     }
-//   });
-// });
-
-// router.delete('/:id', (req, res) => {
-//   const employeeId = req.params.id;
-//   const filteredEmployees = employees.filter(
-//     (employee) => employee.id !== employeeId,
-//   );
-//   fs.writeFile(
-//     'src/data/employees.json',
-//     JSON.stringify(filteredEmployees),
-//     (err) => {
-//       if (err) {
-//         res.send('Cannot delete employee');
-//       } else {
-//         res.send('Employee deleted');
-//       }
-//     },
-//   );
-// });
-
 import Employees from '../models/employees';
 
 const getAllEmployees = async (req, res) => {
   try {
     const employees = await Employees.find();
+
+    if (employees.length === 0) {
+      return res.status(404).json({
+        message: 'No employees found',
+        data: undefined,
+        error: true,
+      });
+    }
 
     return res.status(200).json({
       message: 'Employee found',
@@ -109,7 +18,7 @@ const getAllEmployees = async (req, res) => {
       error: false,
     });
   } catch (error) {
-    return res.json({
+    return res.status(400).json({
       message: 'An error occurred',
       data: undefined,
       error: true,
@@ -128,8 +37,8 @@ const getEmployeeById = async (req, res) => {
       error: false,
     });
   } catch (error) {
-    return res.json({
-      message: 'An error occurred',
+    return res.status(404).json({
+      message: 'Invalid ID',
       data: undefined,
       error: true,
     });
@@ -153,7 +62,7 @@ const createEmployee = async (req, res) => {
       error: false,
     });
   } catch (error) {
-    return res.json({
+    return res.status(400).json({
       message: 'An error occurred',
       data: undefined,
       error: true,
