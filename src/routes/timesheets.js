655a511e3d8e8852cb1ed04c7fc7f92ab015e56e
Fabian Dutra Silva <fabiandutrasilva@gmail.com>
2022-10-17 22:33:27 -0300

    TG-13: add edit validation. Update postman collection with put validation failure

--- src/routes/timesheets.js
+++ src/routes/timesheets.js
@@ -5,10 +5,10 @@ import timesheetsValidation from '../validations/timesheets';
 const router = express.Router();
 
 router
-  .post('/', timesheetsValidation.validateCreation, timesheetsControllers.createTimesheet)
+  .post('/', timesheetsValidation.validateCreationAndUpdate, timesheetsControllers.createTimesheet)
   .get('/', timesheetsControllers.getAllTimesheets)
   .get('/:id', timesheetsControllers.getTimesheetById)
-  .put('/:id', timesheetsControllers.editTimesheetById)
+  .put('/:id', timesheetsValidation.validateCreationAndUpdate, timesheetsControllers.editTimesheetById)
   .delete('/:id', timesheetsControllers.deleteTimesheetById);
 
 export default router;
--- src/validations/timesheets.js
+++ src/validations/timesheets.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 import { now } from 'mongoose';
 
-const validateCreation = (req, res, next) => {
+const validateCreationAndUpdate = (req, res, next) => {
   const timesheetValidation = Joi.object({
     description: Joi.string().min(3).max(50).required(),
     date: Joi.date().max(now().toDateString()).required(),
@@ -21,5 +21,5 @@ const validateCreation = (req, res, next) => {
 };
 
 export default {
-  validateCreation,
+  validateCreationAndUpdate,
 };
