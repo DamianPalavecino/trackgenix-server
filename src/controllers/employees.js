00e278e0d1d320659041ba51663cd6cd3b1aea98
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-10-18 23:08:25 -0300

    TG: Add final correction

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -4,7 +4,7 @@ const deleteEmployee = async (req, res) => {
   try {
     const { id } = req.params;
     const result = await Employees.findByIdAndDelete(id);
-    if (!result) {
+    if (result === null) {
       return res.status(404).json({
         message: 'Employee not found',
         data: undefined,
@@ -27,12 +27,25 @@ const deleteEmployee = async (req, res) => {
 const editEmployee = async (req, res) => {
   try {
     const { id } = req.params;
+    if (Object.entries(req.body).length === 0) {
+      return res.status(400).json({
+        message: 'You must edit at least one field',
+        data: undefined,
+        error: true,
+      });
+    }
     const result = await Employees.findByIdAndUpdate(
       { _id: id },
       { ...req.body },
       { new: true },
     );
-
+    if (result === null) {
+      return res.status(404).json({
+        message: 'Employee not found',
+        data: undefined,
+        error: true,
+      });
+    }
     return res.status(200).json({
       message: `Employee widh id ${id} edited`,
       data: result,
