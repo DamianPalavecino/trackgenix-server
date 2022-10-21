095e9a530f3f3d1f28aa29aebb6a88fcb9a1704a
juancapp <juanitocamo3@gmail.com>
2022-10-21 18:07:19 -0300

    TG: remove super-admin.js models file

--- src/index.js
+++ src/index.js
@@ -14,10 +14,13 @@ mongoose.connect(
   MONGO_URL,
   (error) => {
     if (error) {
+      // eslint-disable-next-line no-console
       console.log('Fail connection to database', error);
     } else {
+      // eslint-disable-next-line no-console
       console.log('Connected to database');
       app.listen(port, () => {
+        // eslint-disable-next-line no-console
         console.log(`Server ready on port ${port}`);
       });
     }
