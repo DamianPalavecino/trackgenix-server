934df662c7f0972e3fda8fb184a63a8a1196a9b9
Tibor takacs <111072050+Tibor700@users.noreply.github.com>
2022-10-21 23:38:35 +0200

    TG-5:add models in capitalize

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -1,73 +1,73 @@
-// import Admins from '../models/admins';
+import Admins from '../models/Admins';
 
-// const editAdmin = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const updatedAdmin = req.body;
+const editAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedAdmin = req.body;
 
-//     if (id === null) {
-//       return res.status(400).json({
-//         message: 'no id parameter',
-//         data: undefined,
-//         error: true,
-//       });
-//     }
+    if (id === null) {
+      return res.status(400).json({
+        message: 'no id parameter',
+        data: undefined,
+        error: true,
+      });
+    }
 
-//     await Admins.findByIdAndUpdate(id, updatedAdmin);
-//     const result = await Admins.findById(id);
+    await Admins.findByIdAndUpdate(id, updatedAdmin);
+    const result = await Admins.findById(id);
 
-//     return res.status(201).json({
-//       message: `Admin id  ${id} edited`,
-//       data: result,
-//       error: false,
-//     });
-//   } catch (error) {
-//     return res.status(404).json({
-//       message: `No admin with '${req.params.id}' as an id`,
-//       data: undefined,
-//       error: true,
-//     });
-//   }
-// };
+    return res.status(201).json({
+      message: `Admin id  ${id} edited`,
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: `No admin with '${req.params.id}' as an id`,
+      data: undefined,
+      error: true,
+    });
+  }
+};
 
-// const deleteAdmin = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await Admins.findByIdAndDelete(id);
+const deleteAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Admins.findByIdAndDelete(id);
 
-//     if (id === null) {
-//       return res.status(400).json({
-//         message: 'no id parameter',
-//         data: undefined,
-//         error: true,
-//       });
-//     }
+    if (id === null) {
+      return res.status(400).json({
+        message: 'no id parameter',
+        data: undefined,
+        error: true,
+      });
+    }
 
-//     await Admins.findByIdAndDelete(id);
+    await Admins.findByIdAndDelete(id);
 
-//     if (result === null) {
-//       return res.status(404).json({
-//         message: 'Admin not found',
-//         data: undefined,
-//         error: true,
-//       });
-//     }
+    if (result === null) {
+      return res.status(404).json({
+        message: 'Admin not found',
+        data: undefined,
+        error: true,
+      });
+    }
 
-//     return res.status(200).json({
-//       message: `Admin with id ${id} deleted`,
-//       data: result,
-//       error: false,
-//     });
-//   } catch (error) {
-//     return res.status(404).json({
-//       message: `No admin with '${req.params.id}' as an id`,
-//       data: undefined,
-//       error: true,
-//     });
-//   }
-// };
+    return res.status(200).json({
+      message: `Admin with id ${id} deleted`,
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: `No admin with '${req.params.id}' as an id`,
+      data: undefined,
+      error: true,
+    });
+  }
+};
 
-// export default {
-//   editAdmin,
-//   deleteAdmin,
-// };
+export default {
+  editAdmin,
+  deleteAdmin,
+};
---
+++ src/models/Admins.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const adminSchema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: { type: String, required: true },
+});
+
+export default mongoose.model('admin', adminSchema);
