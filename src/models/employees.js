25d98183fce372fa30243a70f2d0e1c757770c0b
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-10-21 12:26:28 -0300

    TG: Corrected models employee file name

--- src/models/employees.js
+++ src/models/employees.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const employeeSchema = new Schema({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phone: { type: Number, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  name: { type: String, required: true, lowercase: true },
+  lastName: { type: String, required: true, lowercase: true },
+  phone: { type: Number, required: true, lowercase: true },
+  email: { type: String, required: true, lowercase: true },
+  password: { type: String, required: true, lowercase: true },
 });
 
 export default mongoose.model('Employee', employeeSchema);
