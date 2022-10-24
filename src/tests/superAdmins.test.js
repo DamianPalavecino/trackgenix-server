a0e5b1ce395078dbc034bde39b0bc06b58ddb2c3
Tibor takacs <111072050+Tibor700@users.noreply.github.com>
2022-10-24 05:49:46 +0200

    TG-3:add post test

--- src/tests/superAdmins.test.js
+++ src/tests/superAdmins.test.js
@@ -4,6 +4,13 @@ import app from '../app';
 import SuperAdmins from '../models/Super-admins';
 import superAdminsSeed from '../seeds/superAdmins';
 
+const mockedSuperAdmin = {
+  name: 'Lucas',
+  lastName: 'Salame',
+  email: 'salamelucas@gmail.com',
+  password: 'pacoelflaco123',
+};
+
 beforeAll(async () => {
   await SuperAdmins.collection.insertMany(superAdminsSeed);
 });
@@ -13,5 +20,20 @@ describe('GET/superAdmins', () => {
     expect(response.status).toBe(200);
     expect(response.body.data.length).toBeGreaterThan(0);
     expect(response.body.error).toBe(false);
+  });
+});
+
+describe('POST/superAdmins', () => {
+  test('should create an super admin', async () => {
+    const response = await request(app).post('/superAdmins').send(mockedSuperAdmin);
+    expect(response.status).toBe(201);
+    expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.error).toBe(false);
+  });
+  test('should not create an super admin', async () => {
+    const response = await request(app).post('/superAdmins').send();
+    expect(response.status).toBe(400);
+    expect(response.body.data.length).toBe(0);
+    expect(response.body.error).toBe(true);
   });
 });
