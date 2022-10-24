815f473462592bfd6a13f8fb22337a962a6350dc
Luis Queffelec <queffelecluis@gmail.com>
2022-10-24 16:44:38 -0300

    TG:create test cases for post method

--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -11,14 +11,6 @@ const mockedTask1 = {
 const mockedTask2 = {
   description: '',
 };
-const mockedTask3 = {
-  description: 'see the grass grow',
-};
-const mockedTask4 = {
-  betelgeuse: true,
-};
-
-let taskId;
 
 beforeAll(async () => {
   await Tasks.collection.insertMany(tasksSeed);
@@ -60,7 +52,6 @@ describe('GET /task', () => {
 describe('POST /task', () => {
   test('Should return status 201', async () => {
     const response = await request(app).post('/tasks').send(mockedTask1);
-    taskId = response.body.data._id;
 
     expect(response.status).toBe(201);
   });
@@ -79,61 +70,5 @@ describe('POST /task', () => {
     const response = await request(app).post('/tasks').send();
 
     expect(response.status).toBe(400);
-  });
-});
-
-describe('GET /task', () => {
-  test('Should return status 200 when search a valid id', async () => {
-    const response = await request(app).get(`/tasks/${taskId}`).send();
-
-    expect(response.status).toBe(200);
-  });
-  test('Should return error false when search a valid id', async () => {
-    const response = await request(app).get(`/tasks/${taskId}`).send();
-
-    expect(response.body.error).toBe(false);
-  });
-  test('Should return error true when search a invalid id', async () => {
-    const response = await request(app).get('/tasks/1234567892345678234567sd').send();
-
-    expect(response.body.error).toBe(true);
-  });
-  test('Should return the task required', async () => {
-    const response = await request(app).get(`/tasks/${taskId}`).send();
-
-    expect(response.body.data.description).toBe(mockedTask1.description);
-  });
-});
-
-describe('PUT /task/:id', () => {
-  test('Should edit a task', async () => {
-    const response = await request(app).put(`/tasks/${taskId}`).send(mockedTask3);
-
-    expect(response.status).toBe(201);
-    expect(response.body.data.description).toBe('see the grass grow');
-  });
-  test('Should return status 404 when search for a invalid id', async () => {
-    const response = await request(app).put('/tasks/1234567892345678234567sd').send(mockedTask3);
-
-    expect(response.status).toBe(404);
-  });
-  test('Should return status 400 when send a empty body', async () => {
-    const response = await request(app).put(`/tasks/${taskId}`).send();
-
-    expect(response.status).toBe(400);
-  });
-  test('Should return status 400 when send invalid keys and values', async () => {
-    const response = await request(app).put(`/tasks/${taskId}`).send(mockedTask4);
-
-    expect(response.status).toBe(400);
-  });
-});
-
-describe('DELETE /task/:id', () => {
-  test('Should delete a task', async () => {
-    const response = await request(app).delete(`/tasks/${taskId}`).send();
-
-    expect(response.status).toBe(204);
-    expect(response.body.data).toBe(undefined);
   });
 });
