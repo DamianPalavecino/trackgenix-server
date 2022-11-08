a440293dc2d604ec24bc24b8a4206d82e5a19fc8
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-08 15:39:21 -0300

    TG-46: Fix name, lastName and email JOI validations in SuperAdmins

--- src/validations/super-admins.js
+++ src/validations/super-admins.js
@@ -1,11 +1,15 @@
 import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
+  const regex = /^[a-zA-Z\s]*$/;
   const superAdminsValidation = Joi.object({
-    name: Joi.string().min(1).max(20).required(),
-    lastName: Joi.string().min(1).max(20).required(),
-    email: Joi.string().min(1).max(100).required(),
-    password: Joi.string().min(1).max(100).required(),
+    name: Joi.string().min(2).max(30).pattern(regex)
+      .required(),
+    lastName: Joi.string().min(2).max(30).pattern(regex)
+      .required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+      .required(),
+    password: Joi.string().min(1).max(30).required(),
   });
 
   const validation = superAdminsValidation.validate(req.body);
@@ -21,11 +25,12 @@ const validateCreation = (req, res, next) => {
 };
 
 const validateEdit = (req, res, next) => {
+  const regex = /^[a-zA-Z\s]*$/;
   const superAdminsValidation = Joi.object({
-    name: Joi.string().min(1).max(20),
-    lastName: Joi.string().min(1).max(20),
-    email: Joi.string().min(1).max(100),
-    password: Joi.string().min(1).max(100),
+    name: Joi.string().min(2).max(30).pattern(regex),
+    lastName: Joi.string().min(2).max(20).pattern(regex),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).min(5).max(30),
+    password: Joi.string().min(8).max(30),
   });
 
   const validation = superAdminsValidation.validate(req.body);
